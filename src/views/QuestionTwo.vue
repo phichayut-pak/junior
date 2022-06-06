d08b77ff1fa67606ba06fc60e36daa55052c74bf
tanadol kamthong <tanadolkamthong@gmail.com>
2022-06-06 22:31:52 +0700

    google from

--- src/views/QuestionTwo.vue
+++ src/views/QuestionTwo.vue
@@ -4,8 +4,8 @@
         <input class="p-2 bg-slate-100 rounded-md ml-auto w-3/12" type="text" placeholder="Answer" v-model="Answer" @keyup.enter="check">
         <button class="bg-slate-100 rounded-md p-2 mr-auto border-solid border-slate-900 border-2" @click="check">Check</button>
     </div>
-    <div class="bg-slate-100 rounded-md sm:p-5 w-fit text-center m-auto border-solid border-slate-900 border-2 p-2 sm:pr-32 sm:pl-32 flex gap-2 flex-col" v-show="q2">
-        <h1></h1>
+    <div class="bg-slate-100 rounded-md sm:p-5 w-fit text-center m-auto border-solid border-slate-900 border-2 p-2 sm:pr-32 sm:pl-32 flex gap-2 flex-col mt-3" v-show="q2">
+        <a href="https://forms.gle/bNTyZJxNw5RFVd7E7">แนะนำตัวหน่อยน้าาาาาา เพือรับคำใบพิเศษ</a>
         <h1 class="text-xl block">คำใบ้ต่อไปคือ</h1>
         <button class="bg-sky-100 rounded-md p-2 hover:bg-sky-200" @click="nextQ">coming soon</button>
     </div>
